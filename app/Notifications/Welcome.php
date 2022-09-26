4e236b949faed45a687ecd29dec3f808b2fcda07
MarcMartinezMascarellUOC <marcmartinezmascarell@uoc.edu>
2022-09-26 14:34:35 +0200

    Diseñno contrasena nueva

--- app/Notifications/Welcome.php
+++ app/Notifications/Welcome.php
@@ -10,9 +10,10 @@ class Welcome extends WelcomeNotification
     public function buildWelcomeNotificationMessage(): MailMessage
     {
         return (new MailMessage)
-            ->subject('Bienvenido a' . env('APP_NAME') . '!')
-            ->line(__('Tu empresa te ha invitado para poder acceder al sistema de fichajes'))
-            ->line(__('Por favor, utiliza el siguiente enlace para crear tu contraseña y poder acceder al sistema'))
+            ->subject('Bienvenido a ' . env('APP_NAME') . '!')
+            ->greeting('¡Hola!')
+            ->line(__('Tu empresa te ha invitado para poder acceder al sistema de fichajes.'))
+            ->line(__('Por favor, utiliza el siguiente enlace para crear tu contraseña y poder acceder al sistema.'))
             ->action('Elegir contraseña', $this->showWelcomeFormUrl)
             ->line('Para acceder a la aplicación, deberás hacerlo con tu correo electrónico y la contraseña elegida.')
             ->line('Gracias por usar nuestra app!');
--- resources/views/vendor/welcomeNotification/welcome.blade.php
+++ resources/views/vendor/welcomeNotification/welcome.blade.php
@@ -1,35 +1,64 @@
-<form method="POST">
-    @csrf
 
-    <input type="hidden" name="email" value="{{ $user->email }}"/>
+@extends('layouts.app', ['class' => 'bg-default'])
 
-    <div>
-        <label for="password">{{ __('Contraseña') }}</label>
+@section('content')
+    @include('layouts.headers.guest')
 
-        <div>
-            <input id="password" type="password" class="@error('password') is-invalid @enderror"
-                   name="password" required autocomplete="new-password">
+    <div class="container mt--8 pb-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-7">
+                <div class="card bg shadow border-0">
+                    {{-- <div class="card-header bg-transparent pb-5">
+                        <div class="text-muted text-center mt-2 mb-3"><small>Entra con</small></div>
+                        <div class="btn-wrapper text-center">
+                            <a href="#" class="btn btn-neutral btn-icon">
+                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/github.svg"></span>
+                                <span class="btn-inner--text">{{ __('Github') }}</span>
+                            </a>
+                            <a href="#" class="btn btn-neutral btn-icon">
+                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/google.svg"></span>
+                                <span class="btn-inner--text">{{ __('Google') }}</span>
+                            </a>
+                        </div>
+                    </div> --}}
+                    <h4 class="text-center mt-4">CronoTime</h4>
+                    <div class="card-body px-lg-5 py-lg-5">
+                        <form method="POST">
+                            @csrf
 
-            @error('password')
-            <span>
-                    <strong>{{ $message }}</strong>
-                </span>
-            @enderror
+                            <input type="hidden" name="email" value="{{ $user->email }}"/>
+                            <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }} mb-3">
+                                <div class="input-group input-group-alternative">
+
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror"
+                                    name="password" required autocomplete="new-password" placeholder="Contraseña">
+                                </div>
+                                @error('password')
+                                <span>
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                                <div class="input-group input-group-alternative">
+                                    <input id="password-confirm" class="form-control" type="password" name="password_confirmation" required
+                                    autocomplete="new-password" placeholder="Repite contraseña">
+                                </div>
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" style="display: block;" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Guardar contraseña y entrar') }}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
-    <div>
-        <label for="password-confirm">{{ __('Confirma Contraseña') }}</label>
-
-        <div>
-            <input id="password-confirm" type="password" name="password_confirmation" required
-                   autocomplete="new-password">
-        </div>
-    </div>
-
-    <div>
-        <button type="submit">
-            {{ __('Save password and login') }}
-        </button>
-    </div>
-</form>
+@endsection
