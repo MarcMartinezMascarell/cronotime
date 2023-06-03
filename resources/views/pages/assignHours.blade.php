6f6414d077e6dbb5c92196d0b06759a5af254db2
MarcMartinezMascarell <marcmartinezmascarell@gmail.com>
2023-06-03 15:44:02 +0200

    Fixed bug with special characters on Projects

--- resources/views/pages/assignHours.blade.php
+++ resources/views/pages/assignHours.blade.php
@@ -38,8 +38,8 @@
                                 <?php $iterator = 0 ?>
                                 @foreach($projects as $client => $clientProjects)
                                     <div class="row w-100">
-                                        <h3 class="open-collapse" role="button" data-collapse="<?php echo str_replace(' ', '', $client) ?>">{{$client}}<i class="ml-2 fa fa-chevron-right"></i></h3>
-                                        <div class="d-none" id="<?php echo str_replace(' ', '', $client) ?>">
+                                        <h3 class="open-collapse" role="button" data-collapse="<?php echo preg_replace("/[^a-zA-Z]+/", "", $client) ?>">{{$client}}<i class="ml-2 fa fa-chevron-right"></i></h3>
+                                        <div class="d-none" id="<?php echo preg_replace("/[^a-zA-Z]+/", "", $client) ?>">
                                             @foreach($clientProjects as $project)
                                             <div class="col-xl-3 form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
                                                 <label class="form-control-label" for="input-name">{{ $project->name }}</label>
